33c6e6246ddb8dbdff6b58b2f40415bef935eaa0
Rahul Jaiswal <55907761+githubnagarro@users.noreply.github.com>
2020-09-26 18:11:16 +0530

    Update RunRestartTests.java

--- src/test/java/checkrestart/RunRestartTests.java
+++ src/test/java/checkrestart/RunRestartTests.java
@@ -20,7 +20,7 @@ public class RunRestartTests {
 	public void setUp() throws InterruptedException {
 		// TODO Auto-generated method stub
 		
-		System.setProperty("webdriver.gecko.driver", "./geckodriver-v0.27.0-win64/geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", "/usr/local/geckodriver");
 		FirefoxOptions options = new FirefoxOptions();
 		options.setHeadless(true);
 		 driver= new FirefoxDriver(options);
